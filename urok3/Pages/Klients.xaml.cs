d3391d73bc4050f6fb833fb148d340a092b7edd8
vadimkom02@gmail.com <vadimkom02@gmail.com>
2022-03-30 10:14:59 +0300

    uroki

---
+++ urok3/Pages/Klients.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace urok3.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Klients.xaml
+    /// </summary>
+    public partial class Klients : Page
+    {
+        public Klients()
+        {
+            InitializeComponent();
+        }
+    }
+}
